7683fa917649f6d8e119bb058c8b806b47adccb1
Matt Rice <mattrice12@outlook.com>
2016-09-06 20:21:26 -0500

    Hello world in ruby and JS

---
+++ hello-world/hello_world.js
@@ -0,0 +1,16 @@
+function helloWorld(input) {
+  console.log(input + ".");
+};
+
+function helloPerson(name) {
+  console.log("Hello " + name + ".")
+};
+
+function goToHello(name) {
+  console.log("Go To Hell, " + name + "!")
+};
+
+
+helloWorld("Hello World");
+helloPerson("Bob")
+goToHello("Bob")
---
+++ hello-world/hello_world.rb
@@ -0,0 +1,16 @@
+def hello_world(input)
+  puts input
+end
+
+def hello_person(name)
+  puts "Hello #{name}"
+end
+
+def go_to_hello(name)
+  puts "Go To Hell, #{name}"
+end
+
+
+hello_world("Hello World!")
+hello_person("Bob")
+go_to_hello("Bob")
