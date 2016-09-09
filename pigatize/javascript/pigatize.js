fb7ea999b364ff1f96de812f7330120f20778416
Matt Rice <mattrice12@outlook.com>
2016-09-09 17:44:13 -0500

    pigatize initialization

---
+++ pigatize/javascript/pigatize.js
@@ -0,0 +1,4 @@
+// Note for this exercise, follow these simplified pig latin rules
+// If the first letter is a vowel, add "way" to the end
+// If the first letter is a consonant, move it to the end and add "ay"
+// Hint: Use regex
