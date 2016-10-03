a1bf7efaf5726319b1d897c5ac31b11637b22efd
Matt Rice <mattrice12@outlook.com>
2016-10-03 17:26:46 -0500

    strong-combo working with tests RUBY

--- string-combo/Ruby/string-combo.rb
+++ string-combo/Ruby/string-combo.rb
@@ -8,11 +8,14 @@ class StringCombo
     arr = string.split('')
     new_arr = []
     final_arr = []
-    n = 0
-    while n < arr.length
-      new_arr.push(arr[n])
-      final_arr.push(new_arr.join(''))
-      n += 1
+    arr.each_with_index do |val, index|
+      n = index
+      while n < arr.length
+        new_arr.push(arr[n])
+        final_arr.push(new_arr.join(''))
+        n += 1
+      end
+      new_arr = []
     end
     final_arr.join(', ')
   end
--- string-combo/Ruby/string-combo_test.rb
+++ string-combo/Ruby/string-combo_test.rb
@@ -4,8 +4,13 @@ require_relative 'string-combo'
 
 class StringComboTest < Minitest::Test
 
-  def test_blank
+  def test_while
+    skip
     assert_equal "d, do, dog", StringCombo.new.run("dog")
+  end
+
+  def test_with_iteration
+    assert_equal "d, do, dog, o, og, g", StringCombo.new.run("dog")
   end
 
 end
