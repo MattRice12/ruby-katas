3dcb82183c647095c6cc287925f26fcae28bbd90
Matt Rice <mattrice12@outlook.com>
2016-10-03 14:24:19 -0500

    basic alphabetizer

---
+++ alphabetize/JavaScript/alphabetize.js
@@ -0,0 +1,12 @@
+// Write a JavaScript function that returns a passed string with letters in alphabetical order.
+// Example string : 'webmaster'
+// Expected Output : 'abeemrstw'
+// Assume punctuation and numbers symbols are not included in the passed string.
+
+
+function alphabetize(string) {
+  var newArr = string.split('').sort();
+  console.log(newArr.join(''));
+};
+
+alphabetize("webmaster is here")
