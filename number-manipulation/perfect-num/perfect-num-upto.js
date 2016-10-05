57b34f5993052b21d08fef4c694956552816c653
Matt Rice <mattrice12@outlook.com>
2016-10-05 16:21:16 -0500

    JavaScript - Perfect Number Range kata - refactored into 2 functions

--- number-manipulation/perfect-num/perfect-num-upto.js
+++ number-manipulation/perfect-num/perfect-num-upto.js
@@ -1,14 +1,18 @@
-function perfectNum(max) {
+function perfectNumRange(max) {
   var perfectArr = [];
   for (var num = 1; num < max; num++) {
-    var numArr = []
-    for (var j = num - 1; j > 0; j--) {
-      if (num % j === 0) numArr.push(j);
-    }
-    var sum = numArr.reduce((a, b) => a + b, 0);
-    if (sum === num) perfectArr.push(num);
+    if (perfectNum(num) === num) perfectArr.push(num);
   }
   return perfectArr;
-}
+};
 
-console.log(perfectNum(40000));
+function perfectNum(num) {
+  var numArr = []
+  for (var j = num - 1; j > 0; j--) {
+    if (num % j === 0) numArr.push(j);
+  }
+  var sum = numArr.reduce((a, b) => a + b, 0);
+  return sum
+};
+
+console.log(perfectNumRange(500));
